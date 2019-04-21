9bacf4049992ab452a4ab713b4070968c1e615e5
Incfex <kuro@kuroa.me>
2019-04-21 17:30:48 -0400

    Added basic lock mech

--- hw7.cc
+++ hw7.cc
@@ -1,6 +1,16 @@
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
 #include <thread>
 using namespace std;
+
+mutex              mtx;
+condition_variable cv;
+bool               ready = false;
+
+void PartWorker(int a) {}
+
+void ProdWorker(int a) {}
 
 int main() {
     const int m = 20, n = 20;
@@ -8,14 +18,15 @@ int main() {
     thread    prodW[n];
     for (int i = 0; i < n; i++) {
         partW[i] = thread(PartWorker, i);
-        prodW[i] = thread(ProductWorder, i);
+        prodW[i] = thread(ProdWorker, i);
     }
-    for (int i = 0; i < m; i++) { partW[i] = thread(PartWorker, i); }
+    for (int i = n; i < m; i++) { partW[i] = thread(PartWorker, i); }
 
     for (int i = 0; i < n; i++) {
         partW[i].join();
         prodW[i].join();
     }
+    for (int i = n; i < m; i++) { partW[i].join(); }
 
     cout << "Finish!" << endl;
 
